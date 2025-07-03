8a58a6ec582cb68e140f8fe12a9c7b9686a2bb00
Shebanti Haldar <shebantih@gmail.com>
2025-07-03 20:48:11 +0530

    Continuing with writing the opcode into python  format

--- Graphics.py
+++ Graphics.py
@@ -1,10 +1,30 @@
 import pygame
-scale = 20
-pygame.init
-canvas = pygame.display.set_mode((64*scale, 32*scale))
 
-###def graphics(){
+col = 64
+row = 32
+def construct(self, scale):
 
+    self.scale = scale
+    
+    pygame.init()
+    self.canvas = pygame.display.set_mode((col*scale, row*scale))
+    self.display = [0] * (col * row)
 
+def detectPixel(self, x, y):
+    if x >= col:
+        x -= col
+    elif x < 0:
+        x += col
 
-###}
+    if y >= row:
+        y -= row
+    elif y < 0:
+        y += row
+
+    pixel_location = x + (y * col)
+    self.display[pixel_location] ^= 1
+
+    return not self.display[pixel_location]
+
+def clear(self):
+    self.display = [0] * (col * row)
--- chip8.py
+++ chip8.py
@@ -1,2 +1,38 @@
 import pygame
 
+def instruction(self, opcode):
+    self.pc += 2
+    x = (opcode & 0x0F00 ) >> 8
+    y = (opcode & 0x00F0) >> 4
+
+def mapping(self, opcode):
+    match (opcode & 0xF000):
+        case 0x0000:
+            match (opcode):
+
+                case 0x00E0:
+                    self.renderer.clear()
+                    return
+                
+                case 0x00EE:
+                    self.pc = self.stack.pop()
+                    return
+
+            
+        case 0x1000:
+            self.pc = (opcode & 0xFFF)
+            return
+        case 0x2000:
+            self.stack.push(self.pc)
+            self.pc = (opcode & 0xFFF)
+    
+            return 
+        case 0x3000:
+            if (self.v[x] ==(opcode & 0xFF)):
+
+                self.pc += 2
+            return
+        case 0x4000:
+
+
+        
