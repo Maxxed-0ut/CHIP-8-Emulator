e95461f23e74df0d0a0e71245bc4a48c6632d0ba
Shebanti Haldar <shebantih@gmail.com>
2025-07-05 20:03:59 +0530

    Almost finished with writing code for instructions Need to add audio, bg sprites etc

--- chip8.py
+++ chip8.py
@@ -1,4 +1,6 @@
 import pygame
+import random
+import math
 
 def mapping(self, opcode):
 
@@ -90,24 +92,78 @@ def mapping(self, opcode):
                     self.v[x] = self.v[x]+ self.v[y]
                     return
                 case 0x5:
+                    self.v[0xF] = 0
+
+                    if(self.v[x] > self.v[y]):
+                        self.v[0xF] = 1
+
+                    self.v[x] -= self.v[y]
+
                     return
                 case 0x6:
+                    self.v[x] = (self.v[x] & 0x1)
+
+                    self.v[x] >>= 1
+
                     return
                 case 0x7:
+
+                    self.v[0xF] = 0
+                    if(self.v[y] > self.v[x] ):
+                        self.v[0xF] = 1
+                    self.v[x] = self.v[y] - self.v[x]
+
                     return
                 case 0xE:
+
+                    self.v[0xF] = (self.v[x] & 0x80)
+                    self.v[x] <<= 1
+
                     return
 
            
         case 0x9000:
+
+            if (self.v[x] != self.v[y]):
+                self.pc += 2
+
             return
+        
         case 0xA000:
+
+            self.i = (opcode & 0xFFF)
+            
             return
+        
         case 0xB000:
+
+            self.pc = (opcode & 0xFFF) + self.v[0]
+
             return
+        
         case 0xC000:
+
+            ran = random.randint(0, 0xFF - 1)
+            self.v[x] = ran & (opcode & 0xFF)
+
             return
         case 0xD000:
+
+            width = 8
+            height = (opcode & 0xF)
+
+            self.v[0xF] = 0
+
+            for row in range(0, height):
+
+                sprite = self.memory[self.i + row]
+                
+                for col in range(0, width):
+                    if ((sprite & 0x80)> 0):
+                        if (self.renderer.detectPixel(self.v[x] + col, self.v[y] + row)) :
+                            self.v[0xF] = 1
+
+                sprite <<= 1    
             return
         case 0xE000:
             return
